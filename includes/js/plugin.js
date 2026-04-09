ddba34109628f8802d91c98789efca2db2186df4
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2026-04-09 15:04:03 +0200

    add - if plugins is active, a message is shown

--- includes/js/plugin.js
+++ includes/js/plugin.js
@@ -1,14 +1,26 @@
         var botonInstalar=document.getElementById('instalarTodos')
         var botonDesinstalar=document.getElementById('desinstalarTodos')
+        var seleccionados= [...document.querySelectorAll('.pluginCheckbox:checked')].map(cb=>cb.value)
+        var todos=[...document.querySelectorAll('.pluginCheckbox')].map(cb=>cb.value)
+        function activadoOno(todos) {
+            todos.forEach(slug =>{
+                const p = plugin.plugins.find(p => p.slug === slug);
+                if(p.active){
+                    let mensaje=document.getElementById(slug)
+                    mensaje.classList.remove("desactivado")
+                }
+            })
+        }
+        activadoOno(todos)
+
 
         botonInstalar.addEventListener('click',()=>{
-            console.log("He entrado al boton")
-            var seleccionados= [...document.querySelectorAll('.pluginCheckbox:checked')].map(cb=>cb.value)
+            console.log("He entrado al boton")    
             console.log("Los seleccioados:")
             console.log(seleccionados)
             seleccionados.forEach(slug => {
                 console.log("He entrado al for each")
-                instalarYActivar(slug,slug+"/"+slug+".php")
+                instalarYActivar(slug)
                 .then(() => console.log('¡Listo!'))
                 .catch(console.error);
             });
@@ -35,7 +47,7 @@
             
         });
 
-        async function activarPlugin(pluginFile) {
+        async function activarPlugin(pluginFile,mensaje) {
             console.log("He entrado a la funcion de activar")
             const body = new FormData();
             body.append('action', 'activar_plugin');
@@ -48,7 +60,7 @@
             });
             const data = await res.json();
 
-            if (data.success) {
+            if (data.success) {   
                 console.log('Activado correctamente');
             } else {
                 console.error('Error:', data.data);
@@ -60,14 +72,13 @@
             const installData = await new Promise((resolve, reject) => {
                 wp.updates.installPlugin({ slug, success: resolve, error: reject });
             });
-
             console.log('Todas las propiedades:', JSON.stringify(installData));
 
             // 2. Usar el pluginFile que devuelve WordPress, no construirlo
             const url = new URL(installData.activateUrl);
             const pluginFile = decodeURIComponent(url.searchParams.get('plugin'));
             console.log(pluginFile)
-            await activarPlugin(pluginFile);
+            await activarPlugin(pluginFile,mensaje);
             checkboxes=document.querySelectorAll('.pluginCheckbox:checked')
             await checkboxes.forEach(checkbox=>{
                 checkbox.checked=false
